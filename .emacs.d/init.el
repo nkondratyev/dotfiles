b5eb530d25bf10e61d9e012a06264982443fd009
Nikolay Kondratyev <nkondratyev@yandex.ru>
2012-11-06 03:51:06 +0600

    Initial commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,42 @@
+;; (ido-mode t)
+(column-number-mode t)
+(setq inhibit-splash-screen t)
+
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+(set-language-environment "UTF-8")
+(setq default-input-method "russian-computer")
+(setq show-trailing-whitespace t)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; (add-to-list 'load-path "~/.emacs.d/site-lisp")
+
+;; Bindings
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key [?\C-.] 'next-buffer)
+(global-set-key [?\C-,] 'previous-buffer)
+
+;; (load-theme 'wombat)
+
+(setq package-archives
+      '(("elpa" . "http://tromey.com/elpa/")
+	("gnu" . "http://elpa.gnu.org/packages/")
+	("marmalade" . "http://marmalade-repo.org/packages/")))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((encoding . utf-8) (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
